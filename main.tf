a248a5a649f22c588a7bea1d4cc57c688f0a5cc8
viv-garot <vivien@hashicorp.com>
2021-07-27 21:17:43 +0200

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,28 @@
+variable "aws_instance_number" {
+  default     = 2
+  type        = string
+  description = "Number of instances to deploy on AWS"
+}
+
+
 provider "aws" {
-    region     = "eu-central-1"
+  region = "eu-central-1"
 }
 
-resource "aws_instance" "server1" {
-    ami           = "ami-05fa05752fc432eeb" # bionic64
-    #ami           = "ami-091d856a5f5701931" # nginx64
-    instance_type = "t2.micro"
-    tags = {
-        "Name" = "server1"
-    }
-}
-
-resource "aws_instance" "server2" {
-    ami           = "ami-05fa05752fc432eeb"
-    instance_type = "t2.micro"
-    tags = {
-        "Name" = "server2"
-    }
+resource "aws_instance" "server" {
+  ami           = "ami-05fa05752fc432eeb" # bionic64
+  instance_type = "t2.micro"
+  count         = var.aws_instance_number
+  tags = {
+    "Name" = "server${count.index}"
+  }
 }
 
 resource "null_resource" "null" {
-    triggers = {
-    # Changes to any ami instance requires re-provisioning of this null_resource
-        instance_ami_ids = "${join(",", [aws_instance.server1.ami, aws_instance.server2.ami])}"
-    }
+    count = var.aws_instance_number
 
-    # As a result of re-createing the null_resource the provisioners are re-run
-    provisioner "local-exec" {
-      command = "echo ami in one of two instances had changed. Server1 public_dns is: ${aws_instance.server1.public_dns}"
-    }
+  # As a result of re-createing the null_resource the associated provisioners are re-run
+  provisioner "local-exec" {
+    command = "echo ${element(aws_instance.server.*.tags.Name, count.index)}, ${element(aws_instance.server.*.public_dns, count.index)}, ${element(aws_instance.server.*.public_ip, count.index)} >> server_details.txt"
+  }
 }
